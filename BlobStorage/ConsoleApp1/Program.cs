dd06af44d28a86a215ddd6683a4cf25a39ada900
adotkang07 <adotkang07@gmail.com>
2022-09-10 17:13:29 -0700

    blob storage console app

---
+++ BlobStorage/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("BLOB STORAGE");
+            ProcessAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Press any key to exit the sample application.");
+            Console.ReadLine();
+        }
+
+        private static async Task ProcessAsync()
+        {
+            string storageConnectionString = "BLOBSTORAGECONNECTIONSTRING";
+
+            // Check whether the connection string can be parsed.
+            CloudStorageAccount storageAccount;
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                CloudBlobContainer cloudBlobContainer =
+                cloudBlobClient.GetContainerReference("dev");
+                await cloudBlobContainer.CreateIfNotExistsAsync();
+
+                BlobContainerPermissions permissions = new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                };
+                await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+                string localPath = @"C:\blob";
+                string localFileName = "blobtest.txt";
+                string sourceFile = Path.Combine(localPath, localFileName);
+                // Write text to the file.
+       
+
+
+                // Get a reference to the blob address, then upload the file to the blob.
+                // Use the value of localFileName for the blob name.
+     
+                //CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(localFileName);
+                //await cloudBlockBlob.UploadFromFileAsync(sourceFile);
+
+                Console.WriteLine("List blobs in container.");
+                BlobContinuationToken blobContinuationToken = null;
+                do
+                {
+                    var results = await cloudBlobContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                    // Get the value of the continuation token returned by the listing call.
+                    blobContinuationToken = results.ContinuationToken;
+                    foreach (IListBlobItem item in results.Results)
+                    {
+                        Console.WriteLine(item.Uri);
+                    }
+                } while (blobContinuationToken != null);
+
+            }
+            else
+            {
+                // Otherwise, let the user know that they need to define the environment variable.
+                Console.WriteLine(
+                    "A connection string has not been defined in the system environment variables. " +
+                    "Add an environment variable named 'CONNECT_STR' with your storage " +
+                    "connection string as a value.");
+                Console.WriteLine("Press any key to exit the application.");
+                Console.ReadLine();
+            }
+          
+
+
+        }
+    }
+}
